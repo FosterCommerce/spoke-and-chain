6083564b2a9ee26fce2dbfeb89cbe45d54360789
shinybrad <shinybrad@users.noreply.github.com>
2024-12-22 00:32:15 +0000

    Apply automatic changes

--- seed.sql
+++ seed.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce87871e3cab817732a426846b0fce9e4280a5e1740b8cf6730c0b63e57c66d4
-size 3370095
+oid sha256:95e423b06d783b8bbc6d5dbca6944950624befdfdeae4ed9950b78643e3a54ec
+size 3372992
